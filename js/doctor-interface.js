4189d1bc3e45cf6dede40aeec541cb21e6976d8c
ReneeMei <meiqianye@gmail.com>
2017-03-17 09:11:14 -0700

    create html form, back-end script and front-end script to test the api to make sure it works

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,3 +1,10 @@
+var Doctor = require("./../js/doctor.js").doctorModule;
+
 $(document).ready(function(){
-  
+ $("form").submit(function(event){
+   event.preventDefault();
+   var medicalIssue = $("#medical-issues").val();
+   var doctor = new Doctor();
+   doctor.GetDoctors(medicalIssue);
+ })
 })
