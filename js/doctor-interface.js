46004bf5aa4c81379217a242b656b6bca9cd7927
ReneeMei <meiqianye@gmail.com>
2017-03-17 09:52:18 -0700

    add script to get doctor's profile

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,10 +1,19 @@
 var Doctor = require("./../js/doctor.js").doctorModule;
+var doctor = new Doctor();
+
+var displayDoctors = function(results){
+  results.forEach(function(doctor){
+    $("#doctor-name").append("<li class='doctors'>"+ doctor.profile.first_name + " " + doctor.profile.last_name + "</li>");
+  });
+  $(".doctors").click(function(){
+    doctor.GetProfile($(this).html());
+  });
+};
 
 $(document).ready(function(){
  $("form").submit(function(event){
    event.preventDefault();
    var medicalIssue = $("#medical-issues").val();
-   var doctor = new Doctor();
-   doctor.GetDoctors(medicalIssue);
- })
-})
+   doctor.GetDoctors(medicalIssue, displayDoctors);
+ });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -4,8 +4,18 @@ function Doctor(){
 
 }
 
-Doctor.prototype.GetDoctors = function (medicalIssue) {
+Doctor.prototype.GetDoctors = function (medicalIssue, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      displayDoctors(result.data);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
+};
+
+Doctor.prototype.GetProfile = function (name) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?name='+ name +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
     })
